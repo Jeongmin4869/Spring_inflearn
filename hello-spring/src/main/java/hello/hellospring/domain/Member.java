8c0a4da8a5b4749f7525fb01dfe2a95e98e4b45f
jeongmin4869 <ljm4135@naver.com>
2023-01-22 00:16:31 +0900

    Spring : JPA 세팅

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 시스템에 저장할때 사용될 ID. 1, 2, 3, .. (시스템이 정해주는 값)
+//    @Column(name = "columnName")
     private String name;
 
     public Long getId() {
